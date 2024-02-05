e00a85f5bdc166379f889d0900d89c39428e720a
Andres M <marulandaandres19@gmail.com>
2024-02-05 10:04:49 -0500

    .env and graphql client

--- App.jsx
+++ App.jsx
@@ -1,30 +1,34 @@
 import { Platform, SafeAreaView, StatusBar, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { QueryClient, QueryClientProvider, useQuery } from '@tanstack/react-query';
 
 import Home from './src/screens/Home';
 import ExerciseDetails from './src/screens/ExerciseDetails';
 
 const Stack = createNativeStackNavigator();
+const client = new QueryClient();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{ headerTitleAlign: 'center' }}
-        />
-        <Stack.Screen
-          name="ExerciseDetails"
-          component={ExerciseDetails}
-          options={{
-            headerTitleAlign: 'center',
-          }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <QueryClientProvider client={client}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{ headerTitleAlign: 'center' }}
+          />
+          <Stack.Screen
+            name="ExerciseDetails"
+            component={ExerciseDetails}
+            options={{
+              headerTitleAlign: 'center',
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </QueryClientProvider>
   );
 }
 
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,13 +1,60 @@
-import { FlatList, StyleSheet, Text, View } from 'react-native';
-import exercises from '../../assets/data/exercises.json';
+import {
+  ActivityIndicator,
+  FlatList,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import ExerciseList from '../components/ExerciseList';
+import { useQuery } from '@tanstack/react-query';
+import { gql, request } from 'graphql-request';
+
+const exercisesQuery = gql`
+  query exercises($muscle: String, $name: String) {
+    exercises(muscle: $muscle, name: $name) {
+      name
+      muscle
+      instructions
+      equipment
+    }
+  }
+`;
+const url = process.env.EXPO_PUBLIC_URL;
+const apikey = process.env.EXPO_PUBLIC_GRAPHQL_API_KEY;
 
 const Home = () => {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['exercises'],
+    queryFn: async () => {
+      try {
+        const result = await request({
+          url,
+          document: exercisesQuery,
+          requestHeaders: {
+            Authorization: `apikey ${apikey}`,
+          },
+        });
+
+        return result;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  });
+
+  if (isLoading) {
+    return <ActivityIndicator />;
+  }
+
+  if (error) {
+    return <Text>Failed to fetch exercises</Text>;
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Workouts</Text>
       <FlatList
-        data={exercises}
+        data={data?.exercises}
         renderItem={({ item }) => <ExerciseList item={item} />}
         keyExtractor={(item) => item.name}
       />
